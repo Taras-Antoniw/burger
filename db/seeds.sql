69c39e8332b7fc8317d970e56b0eba7dc3bfd560
PorchDriver <tantoniw@cogeco.ca>
2019-01-21 18:11:56 -0500

    initial stuff

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bronto Burger');
+INSERT INTO burgers (burger_name) VALUES ('Dino Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Works Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Fat Burger');
